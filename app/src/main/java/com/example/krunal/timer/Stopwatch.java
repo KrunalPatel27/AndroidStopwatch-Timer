09da7c33ae0a2dbe080c8a8536de9917df512fd7
KrunalPatel27 <kpate236@gmail.com>
2017-12-31 13:47:25 -0600

    stopwatch lap time feature added

--- app/src/main/java/com/example/krunal/timer/Stopwatch.java
+++ app/src/main/java/com/example/krunal/timer/Stopwatch.java
@@ -5,20 +5,23 @@ import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.sql.Time;
-import java.util.Timer;
 
 public class Stopwatch extends AppCompatActivity {
-
+    ListView listView;
     Button button1, button2;
-    TextView textView;
+    TextView textView, lapTimeView;
     int Seconds, Minutes, MilliSeconds ;
     boolean stopwatchOFFState = true;
     long StopWatchTime, MillisecondTime, StartTime, UpdateTime = 0L ;
     Handler handler;
+    List ListElementsArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +31,15 @@ public class Stopwatch extends AppCompatActivity {
         button1 = (Button)findViewById(R.id.button1);
         button2 = (Button)findViewById(R.id.button2);
         textView =(TextView)findViewById(R.id.timeView);
+        lapTimeView = (TextView)findViewById(R.id.lapTimeView);
+        listView = (ListView)findViewById(R.id.list1);
 
         handler = new Handler();
+
+        ListElementsArrayList = new ArrayList<String>();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, ListElementsArrayList );
+        listView.setAdapter(adapter);
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,9 +72,19 @@ public class Stopwatch extends AppCompatActivity {
                     UpdateTime = 0L ;
                     button1.setText("Lap");
                     textView.setText("00:00.00");
+                    lapTimeView.setText("00:00.00");
                     button1.setEnabled(false);
-                }else{
+                    //clear lap time info-- ListView
+                    ListElementsArrayList.clear();
+                    adapter.notifyDataSetChanged();
 
+                }else{
+                    //If Lap is clicked
+                    ListElementsArrayList.add(0, getStringTime(UpdateTime));
+                    MillisecondTime +=  UpdateTime;
+                    StartTime = SystemClock.elapsedRealtime();
+                    listView.smoothScrollToPosition(0);
+                    adapter.notifyDataSetChanged();
                 }
             }
         });
@@ -77,19 +97,21 @@ public class Stopwatch extends AppCompatActivity {
             UpdateTime = SystemClock.elapsedRealtime() - StartTime;
             StopWatchTime = MillisecondTime + UpdateTime;
 
-            Seconds = (int) (StopWatchTime / 1000);
-
-            Minutes = Seconds / 60;
-
-            Seconds = Seconds % 60;
-
-            MilliSeconds = (int) (StopWatchTime % 1000);
-            String tmp = "" + String.format("%02d",Minutes) + ":"
-                    + String.format("%02d", Seconds) + "."
-                    + String.format("%02d", MilliSeconds/10);
-            textView.setText(tmp);
+            textView.setText(getStringTime(StopWatchTime));
+            lapTimeView.setText(getStringTime(UpdateTime));
             handler.postDelayed(this, 0);
         }
     };
+
+    public String getStringTime (long Time){
+        Seconds = (int) (Time / 1000);
+        Minutes = Seconds / 60;
+        Seconds = Seconds % 60;
+        MilliSeconds = (int) (Time % 1000);
+
+        return "" + String.format("%02d",Minutes) + ":"
+                + String.format("%02d", Seconds) + "."
+                + String.format("%02d", MilliSeconds/10);
+    }
 
 }
